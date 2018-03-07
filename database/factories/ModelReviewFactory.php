19fd3ebf36a829ebfa3305466847abfaedb4f69d
tanakphong <t.phongsing@gmail.com>
2018-03-07 14:27:56 +0700

    Relationship created

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -3,7 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
-    return [
-        //
+    return [  
+        'product_id' => $faker->numberBetween(1,100),
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
