e3697f19fbbf9d44b12f0acf4fe1be4900160aaf
tanakphong <t.phongsing@gmail.com>
2018-03-12 00:31:13 +0700

    Pasasort Installed

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Turn On The Lights
+| Turn On The Lightsns
 |--------------------------------------------------------------------------
 |
 | We need to illuminate PHP development, so let us turn on the lights.
