c30a54a5f47876a00ace5d5cc2d62f5c87021062
tanakphong <t.phongsing@gmail.com>
2018-03-13 00:20:08 +0700

    Product update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function reviews(){
-    	return $this->hasMany(Review::class);
-    }
+	protected $fillable = [
+		'name','detail','stock','price','discount'
+	];
+
+	public function reviews(){
+		return $this->hasMany(Review::class);
+	}
 }
