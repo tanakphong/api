a1ad0fbe2f68025fac1b1e58d5c810a4f3d9f85f
tanakphong <t.phongsing@gmail.com>
2018-03-15 23:38:33 +0700

    Cuctomised Exception Trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+trait ExceptionTrait{
+	public function apiException($request,$e){
+
+		if($this->isModel($e)){
+			return $this->ModelResponse($e);
+		}
+
+				if ($this->isHttp($e)) {
+					return $this->HttpResponse($e);
+				}
+
+						return parent::render($request, $exception);
+	}
+
+	public function isModel($e){
+		return $e instanceof ModelNotFoundException;
+	}
+
+	public function isHttp($e){
+		return $e instanceof NotFoundHttpException;
+	}
+
+	public function ModelResponse($e){
+		return response()->json([
+			'error' => 'Product Model not found'
+		],Response::HTTP_NOT_FOUND); 
+	}
+
+	public function HttpResponse($e){
+		return response()->json([
+			'error' => 'Incorect route'
+		],Response::HTTP_NOT_FOUND);
+	}
+
+}
